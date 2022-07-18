c9b88fc1036df64202baacb53ebe480924968187
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-07-18 18:29:16 +0530

    .

---
+++ 1/file.c
@@ -0,0 +1,14 @@
+//Write a C program to copy the contents of a text file to another text file.
+#include <stdio.h>
+int main(){
+    FILE *fp1, *fp2;
+    char ch;
+    fp1 = fopen("file1.txt", "r");
+    fp2 = fopen("file2.txt", "w");
+    while((ch = fgetc(fp1)) != EOF){
+        fputc(ch, fp2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+    return 0;
+}
